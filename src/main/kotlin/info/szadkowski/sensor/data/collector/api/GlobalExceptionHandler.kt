b7781e89d543789cf2acb50a4a20603a4d1eadab
Adam Szadkowski <adam@szadkowski.info>
2020-12-12 22:34:54 +0100

    Ensure missing api key causes bad request with defined reason

--- src/main/kotlin/info/szadkowski/sensor/data/collector/api/GlobalExceptionHandler.kt
+++ src/main/kotlin/info/szadkowski/sensor/data/collector/api/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package info.szadkowski.sensor.data.collector.api
 
+import info.szadkowski.sensor.data.collector.domain.MissingApiKeyException
 import info.szadkowski.sensor.data.collector.domain.WriteFailedException
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -12,6 +13,12 @@ class GlobalExceptionHandler {
     @ExceptionHandler(WriteFailedException::class)
     @ResponseStatus(HttpStatus.FAILED_DEPENDENCY)
     fun handleWriteException() {
+        // do nothing
+    }
+
+    @ExceptionHandler(MissingApiKeyException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST, reason = "Incorrect request header 'X-API-KEY'")
+    fun handleMissingApiKeyException() {
         // do nothing
     }
 }
--- src/main/kotlin/info/szadkowski/sensor/data/collector/infrastructure/property/PropertySensorRepository.kt
+++ src/main/kotlin/info/szadkowski/sensor/data/collector/infrastructure/property/PropertySensorRepository.kt
@@ -1,5 +1,6 @@
 package info.szadkowski.sensor.data.collector.infrastructure.property
 
+import info.szadkowski.sensor.data.collector.domain.MissingApiKeyException
 import info.szadkowski.sensor.data.collector.domain.SensorRepository
 import info.szadkowski.sensor.data.collector.domain.model.Sensor
 
@@ -11,5 +12,5 @@ class PropertySensorRepository(
         .mapValues { (_, v) -> v[0] }
         .mapValues { (_, v) -> Sensor(v.location) }
 
-    override fun fetch(apiKey: String): Sensor = sensorsByKey[apiKey]!!
+    override fun fetch(apiKey: String): Sensor = sensorsByKey[apiKey] ?: throw MissingApiKeyException()
 }
