76d0d5942b0f5f99217dec0a0832e060dd15f303
Adam Szadkowski <adam@szadkowski.info>
2022-09-28 16:26:04 +0200

    Update jdk to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "15"
+            jvmTarget = "17"
         }
     }
     withType<Test> {
