69ba781c8bdb0f0c5d0fffb9455b41b9a54000dd
Adam Szadkowski <adam@szadkowski.info>
2023-04-05 23:32:47 +0200

    Fix http metrics

--- src/main/kotlin/info/szadkowski/sensor/data/collector/infrastructure/metrics/SensorVersionObservationConvention.kt
+++ src/main/kotlin/info/szadkowski/sensor/data/collector/infrastructure/metrics/SensorVersionObservationConvention.kt
@@ -2,15 +2,15 @@ package info.szadkowski.sensor.data.collector.infrastructure.metrics
 
 import io.micrometer.common.KeyValue
 import io.micrometer.common.KeyValues
+import org.springframework.http.server.observation.DefaultServerRequestObservationConvention
 import org.springframework.http.server.observation.ServerRequestObservationContext
-import org.springframework.http.server.observation.ServerRequestObservationConvention
 
-class SensorVersionObservationConvention : ServerRequestObservationConvention {
-    override fun getName(): String = "http.server.requests"
-
+class SensorVersionObservationConvention : DefaultServerRequestObservationConvention() {
     override fun getLowCardinalityKeyValues(context: ServerRequestObservationContext): KeyValues =
-        KeyValues.of(
-            KeyValue.of("sensor-version", context.carrier.getHeader("Sensor-Version") ?: "unknown"),
-            KeyValue.of("sensor-type", context.carrier.getHeader("Sensor-Type") ?: "unknown"),
+        super.getLowCardinalityKeyValues(context).and(
+            KeyValues.of(
+                KeyValue.of("sensor-version", context.carrier.getHeader("Sensor-Version") ?: "unknown"),
+                KeyValue.of("sensor-type", context.carrier.getHeader("Sensor-Type") ?: "unknown"),
+            )
         )
 }
